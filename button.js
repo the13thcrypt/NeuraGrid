98fd7692a1d036efde843a8eb0bef53a3dac5135
the13thcrypt <sam0dahiya13@gmail.com>
2024-10-20 18:05:56 +0530

    Add files via upload

---
+++ button.js
@@ -0,0 +1,16 @@
+const syncPointer = (event) => {
+    const pointerX = event.x;
+    const pointerY = event.y;
+
+    const x = pointerX.toFixed(2);
+    const y = pointerY.toFixed(2);
+    const xp = (pointerX / window.innerWidth).toFixed(2);
+    const yp = (pointerY / window.innerHeight).toFixed(2);
+
+    document.documentElement.style.setProperty('--x', x);
+    document.documentElement.style.setProperty('--xp', xp);
+    document.documentElement.style.setProperty('--y', y);
+    document.documentElement.style.setProperty('--yp', yp);
+};
+
+document.body.addEventListener('pointermove', syncPointer);
